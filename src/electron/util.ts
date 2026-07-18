a81817c5d89147efe5d1d237c42fb22c0818c46e
Fernando Nogueira <fernando.cossta@gmail.com>
2026-07-18 14:16:36 -0300

    Add helpers and components json file

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function isDev(): boolean {
-  return process.env.NODE_ENV === 'development';
+  return process.env.NODE_ENV === "development";
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
